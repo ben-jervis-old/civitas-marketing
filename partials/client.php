9d345a263bc2903d2269f585ced76f9955fb5b79
benJervis <ben@jervis.net.au>
2017-06-02 15:52:40 +1000

    Updates to Client section

--- partials/client.php
+++ partials/client.php
@@ -8,11 +8,24 @@
 			</div>
 		</div>
 		<div class="row">
-				<div class="text-center">
-					<p>
-						<span class="inline-brand">civitasCRM</span> was developed for and in conjunction with <a href="http://stmarksww.org.au/" target="_blank">St Mark's Anglican Church West Wollongong</a>.
-					</p>
-				</div>
+			<div class="col-md-4">
+				<h4>St Mark's</h4>
+				<p>
+					<span class="inline-brand">civitasCRM</span> was developed for and in conjunction with <a href="http://stmarksww.org.au/" target="_blank">St Mark's Anglican Church West Wollongong</a>. Having outgrown the effectiveness of their current management tools, St Mark’s required a new way to manage their interactions with the 250-300 members of the congregation.
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h4>Issue</h4>
+				<p>
+					St Mark's current system lacked portability and reliability, and was difficult to use. All members of the congregation should be able to use this system, and they should be able to use it from anywhere. The existing paper based system for taking attendance was inefficient and unreliable.
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h4>Solution</h4>
+				<p>
+					To address these issues, we developed <span class="inline-brand"><a href="http://app.civitascrm.com.au/" target="_blank">civitasCRM</a></span>, a web application that would be portable, reliable, and emphasis user friendliness as a core concept. <span class="inline-brand">civitasCRM</span> will provide a new way for St Mark's to interact with the congregation.
+				</p>
+			</div>
 		</div>
 	</div>
 </section>
