1f339c63162937e7339cad65066e9faa46941251
benJervis <ben@jervis.net.au>
2017-06-02 19:23:41 +1000

    Product section

--- partials/about.php
+++ partials/about.php
@@ -1,18 +1,18 @@
 <section class="success" id="about">
 		<div class="container">
 				<div class="row">
-						<div class="col-lg-12 text-center">
+						<div class="col-md-12 text-center">
 								<h2>About Us</h2>
 								<hr class="star-light">
 						</div>
 				</div>
 				<div class="row">
-						<div class="col-lg-4 col-lg-offset-2">
+						<div class="col-md-4 col-md-offset-2">
 								<p>
 									The <?php inline_brand(); ?> team are a group of four students from the <a href="http://www.uow.edu.au/index.html">University of Wollongong</a>'s School of Computing and Information Technology.
 								</p>
 						</div>
-						<div class="col-lg-4">
+						<div class="col-md-4">
 								<p>
 									The system being developed constitutes a year long capstone project for subject CSIT321 undertaken by all final year students.
 								</p>
--- partials/product.php
+++ partials/product.php
@@ -1,27 +1,35 @@
 <section class="success" id="product">
-		<div class="container">
-				<div class="row">
-						<div class="col-lg-12 text-center">
-								<h2>The Product</h2>
-								<hr class="star-light">
-						</div>
-				</div>
-				<div class="row">
-						<div class="col-lg-4 col-lg-offset-2">
-								<p>
-									Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets for easy customization.
-								</p>
-						</div>
-						<div class="col-lg-4">
-								<p>
-									Whether you're a student looking to showcase your work, a professional looking to attract clients, or a graphic artist looking to share your projects, this template is the perfect starting point!
-								</p>
-						</div>
-						<div class="col-lg-8 col-lg-offset-2 text-center">
-								<a href="http://app.civitascrm.com.au/" class="btn btn-lg btn-outline">
-										<i class="fa fa-code"></i> See our live demo
-								</a>
-						</div>
-				</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 text-center">
+				<h2>The Product</h2>
+				<hr class="star-light">
+			</div>
 		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				<p>
+					<?php inline_brand(); ?> is a Client Relationship Management tool, built in <a href="http://rubyonrails.org/" target="_blank">Ruby on Rails</a>, that emphasises strong personal connections between staff and members. Intended for small to medium sized groups, <?php inline_brand(); ?> focuses on providing a way for people to interact with their organisation in personal and meaningful ways. It provides a reliable, portable, and usable way to stay connected with your members, keep track of contact details, and make sure no one falls through the cracks.
+				</p>
+			</div>
+			<div class="col-lg-4">
+				<p>
+				Very small groups can handle group contact and management with manual methods like spreadsheets and email lists, while very large groups can use specialised packages designed for their scale and price point. However, there are limited options to cover this middle ground, groups who are too big to keep track of things manually, but have no need to store 2000 members, or use many of the advanced and complex features that exist in large scale CRM packages.
+				</p>
+			</div>
+			<div class="col-lg-4">
+				<p>
+					Whether you're a religious organisiation like St Mark's, a local sporting club, or a volunteer community organisation, <?php inline_brand(); ?> can help you bring a professional level of organisation to your group, without losing that small-group personal feeling.<br><br>To find out more, you can drop us a <span class="page-scroll"><a href="#contact">line</a></span>.
+				</p>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 text-center">
+				<a href="http://app.civitascrm.com.au/" class="btn btn-lg btn-outline">
+					<i class="fa fa-code"></i> See our live demo
+				</a>
+			</div>
+		</div>
+	</div>
 </section>
